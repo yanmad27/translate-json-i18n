ec86f95ec8e0fc8b34fb24103c8bbfd58b6bba29
Nick Vujicic <nick.vujicic@abc.xyz>
2023-11-28 20:55:03 +0700

    feat: recursion obj translate

--- back-end/index.js
+++ back-end/index.js
@@ -1,39 +1,41 @@
 const express = require('express')
 const app = express()
-const fs = require('fs');
-var cors = require('cors')
+const cors = require('cors')
 
-const i18nPath = 'i18n'
-const translate = async (target, text) => {
+const translateWord = async (target, text) => {
     const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=${target}&dt=t&q=${text}`
     return fetch(url).then(res => res.json()).then(res => res[0][0][0])
 }
-const toCapitalize = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+const toCapitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+const translate = async (target, curr) => {
+    const keys = Object.keys(curr);
+    const result = {}
+    for (let key of keys) {
+        const isArray = Array.isArray(curr[key])
+        const dataType = isArray ? 'array' : typeof curr[key]
+
+        switch (dataType) {
+            case "string":
+                result[key] = toCapitalize(await translateWord(target, curr[key]));
+                break;
+            case "object":
+                result[key] = await translate(target, curr[key]);
+                break;
+            default:
+                result[key] = curr[key];
+
+        }
+    }
+    return result
 }
-
-const translateAll = async (targets, json) => {
-
-
-    console.log('LOG ~ :', targets);
-    const enObject = json
-    const enKeys = Object.keys(enObject);
-
-    const total = targets.length;
-    let index = 1;
+const translateAll = async (targets,) => {
 
     const results = {}
 
     for (let target of targets) {
-        let result = {}
-        for (let key of enKeys) {
-            result[key] = toCapitalize(await translate(target, enObject[key]));
-        }
-
-        result = JSON.stringify(result, null, 2)
-
-        results[target] = result
-        console.log('LOG ~ :', target + '.json', index++, '/', total, 'done');
+        const result = await translate(target, json)
+        results[target] = JSON.stringify(result, null, 2)
     }
 
     return results
